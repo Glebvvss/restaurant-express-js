3818733387f27280922a14cbae078b662e0256dd
Gleb Vasylenko <hleb.v@platinumlist.pro>
2020-09-29 13:43:56 +0300

    REFACTORING: Use 2 tab spaces in migrations

--- src/migrations/0_schema.js
+++ src/migrations/0_schema.js
@@ -2,22 +2,22 @@ const dbConfig = require('../config/db.js');
 const sqlite   = require('sqlite-async')
 
 sqlite
-    .open(dbConfig.dbname)
-    .then(async db => {
-        await db.run(`
-            CREATE TABLE tables (
-                table_number INTEGER PRIMARY KEY,
-                capacity     INTEGER
-            )
-        `)
-        await db.run(`
-            CREATE TABLE reserves (
-                id           INTEGER PRIMARY KEY AUTOINCREMENT,
-                table_number INTEGER,
-                date         TEXT,
-                client_name  TEXT,
-                FOREIGN KEY (table_number) REFERENCES tables(table_number)
-            )
-        `)
-    })
-    .catch(console.log)
+  .open(dbConfig.dbname)
+  .then(async db => {
+    await db.run(`
+      CREATE TABLE tables (
+        table_number INTEGER PRIMARY KEY,
+        capacity     INTEGER
+      )
+    `)
+    await db.run(`
+      CREATE TABLE reserves (
+        id           INTEGER PRIMARY KEY AUTOINCREMENT,
+        table_number INTEGER,
+        date         TEXT,
+        client_name  TEXT,
+        FOREIGN KEY (table_number) REFERENCES tables(table_number)
+      )
+    `)
+  })
+  .catch(console.log)
